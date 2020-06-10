be76d79a81dea2252ec1d73fb80d07d3c59b57b0
StrangerZiokan <46660993+StrangerZiokan@users.noreply.github.com>
2020-06-10 20:27:42 +0530

    Add files via upload

---
+++ weel1/lcm_w2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+long long euclid_fast(long long a, long long b) {
+
+  if(b == 0){
+        return a;
+  }
+
+  long long a1 = a%b;
+
+  return euclid_fast(b,a1);
+
+}
+
+long long lcm_fast(long long a, long long b) {
+
+    long long i = euclid_fast(a,b);
+
+    long long lcm = (a*b)/i;
+
+    return lcm;
+  }
+
+int main() {
+  long long a, b;
+  std::cin >> a >> b;
+  std::cout << lcm_fast(a, b) << std::endl;
+  return 0;
+}
